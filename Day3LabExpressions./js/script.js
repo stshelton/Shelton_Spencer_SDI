ba8cb9d93dbdf7b251224cab0290017ae0ad601d
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-05 18:49:36 -0500

    question 1

---
+++ Day3LabExpressions./js/script.js
@@ -0,0 +1,29 @@
+/*
+ Spencer Shelton
+ 3/5/15
+ section 00
+ day 2 intro
+ */
+
+// slices of pie part 1
+
+var SlicesPerPizza = 10;
+var PartyPeople = 10;
+var PizzasOrdered= 10;
+
+var slicesPerPerson = SlicesPerPizza/PartyPeople;
+
+//phrases
+var phrase1 ="Each person ate ";
+var phrase2 =" slices of pizza at the party";
+//cout <<
+console.log(phrase1,slicesPerPerson,phrase2);
+
+
+
+
+
+
+
+
+
