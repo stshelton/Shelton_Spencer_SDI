2220c1d87db5071bfccc45cdcc3dfa1777dc28d7
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-05 14:54:11 -0500

    day two intro files starting
    creating html and js file

---
+++ day 2/js/script.js
@@ -0,0 +1,21 @@
+/*
+Spencer Shelton
+3/5/15
+section 00
+day 2 intro
+ */
+
+//alert("Testing to see if connected");
+
+/*
+example of a multi-line comment
+anything inside of here will not be seen by the computer
+ */
+
+//single line comment. ONLY this line will be ignored
+
+/*Alert box - pop up box that will alert the user with information
+alert("text that the user will see");
+ */
+
+
