9d598f81744253d942532e8f13493bd030f10709
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-05 19:09:13 -0500

    question 3 shopping bill

--- Day3LabExpressions./js/script.js
+++ Day3LabExpressions./js/script.js
@@ -30,6 +30,25 @@ console.log(phrase3,slicesSparky,phrase4);
 
 
 
+// shopping bill part 1 
+
+var week1 = 200;
+var week2= 200;
+var week3= 250;
+var week4= 150;
+var week5= 200;
+
+var totalShoppingBill = week1+week2+week3+week4+week5;
+var averageSpending = (week1+week2+week3+week4+week5)/5;
+
+
+// print out onto screen
+console.log("you have spent a total of $ ", totalShoppingBill, " on groceries over 5 weeks. That is an average of $ ", averageSpending," per week ");
+
+
+
+
+
 
 
 
