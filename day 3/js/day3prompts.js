18b2b2abd6886454bc6652fe7c54077409a9eb26
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-07 15:27:53 -0500

    learn to prompt
    have user input

---
+++ day 3/js/day3prompts.js
@@ -0,0 +1,31 @@
+/*
+spencer shelton
+section 00
+3/7/15
+day 3 prompts
+ */
+
+//alert("just testing!")
+
+// cin
+// ask the user for input
+// prompt ("text that the user will see")
+
+var userInput =prompt ("enter year of birth");
+console.log(userInput);
+
+//calculate the area of a rectangle
+//length * width
+var height = prompt("lets calculate the area of a rectangle!\n enter the height: ");
+
+var width = prompt("please enter the width of the rectangle:");
+
+//calculate the ara of the rectangle
+var area = width *height;
+
+//tell the user
+console.log("the area of the rectangle with a height of "+height+" and a width of "+width+" is "+area+"");
+
+var results = "the results of the area are "+area;
+
+console.log(results);
