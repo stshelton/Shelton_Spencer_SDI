3842073bbe5880d88c87a4f4c29fdd0050fb6d04
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-07 18:43:12 -0500

    math for total and average score

--- expressions assignment/js/nba.js
+++ expressions assignment/js/nba.js
@@ -10,9 +10,17 @@
 
 
 // questions to ask user
-var question= prompts("How many points did you score in the 1st quarter?");
+var question = prompts("How many points did you score in the 1st quarter?");
 var question2 = prompts("How many points did you score in the 2nd quarter?");
 var question3 = prompts("How many points did you score in the 3rd quarter?");
 var question4 = prompts("How many points did you score in the 4th quarter?");
+
+//total score
+var totalScore = question+question2+question3+question4;
+//display total score
+console.log("your total score for this basketball game is "+totalScore);
+
+var averageScore = totalScore/4;
+
 
 
