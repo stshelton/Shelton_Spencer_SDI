8bd0a1037fc60704d9f5f1ac45a73c45c4362820
stshelton <spencershelton1@Spencers-MacBook-Pro.local>
2015-03-07 19:22:10 -0500

    add array together

--- expressions assignment/js/nba.js
+++ expressions assignment/js/nba.js
@@ -16,10 +16,10 @@ for(var i=1; i<5;i++ )
     questionArray[i] = prompt("How many points did you score in the" + i + "st quarter?")
 }
 
-for(var i=1; i<5;i++ )
-{
-    totalScore = parseInt(questionArray[i])+parseInt(question2)+parseInt(question3)+parseInt(question4);
-}
+
+//adding the array together
+totalScore = parseInt(questionArray[1])+parseInt(questionArray[2])+parseInt(questionArray[3])+parseInt(questionArray[4]);
+
 //display total score
 console.log("your total score for this basketball game is "+totalScore);
 //average per quarter
