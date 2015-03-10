96a9c4efb73b5dd32ebd68ad971013144b542f76
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-10 18:36:01 -0400

    variables and 1st if statement

---
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -0,0 +1,26 @@
+/*
+ Spencer Shelton
+ 3/10/15
+ section 00
+ conditionals worksheet
+ */
+
+
+// question 1 gas
+
+//variables
+
+var mpg = 30;
+var leftInTank = prompt("approximately how much do you have left in your tank out of a 100?");
+var tankCapacity = 100;
+
+
+//
+if(leftInTank>= 66);
+{
+    console.log("If you have a full tank with"+mpg+"mpg and a tank capacity of"+tankCapacity+" with"+leftInTank+" then you do not need to stop for gas" );
+}
+else
+{
+    console.log ("need gas asap ")
+}
