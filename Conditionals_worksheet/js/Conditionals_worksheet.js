2e176edabce9837820ea861f148cd31cb10a61ea
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-10 18:40:34 -0400

    question 1 done

--- Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -15,12 +15,13 @@ var leftInTank = prompt("approximately how much do you have left in your tank ou
 var tankCapacity = 100;
 
 
-//
-if(leftInTank>= 66);
+// if statement with hard coded mpg and tank capacity you would have to have less then 66% of your tank left to not make it 200 miles
+if(leftInTank>= 66)
 {
     console.log("If you have a full tank with"+mpg+"mpg and a tank capacity of"+tankCapacity+" with"+leftInTank+" then you do not need to stop for gas" );
 }
 else
 {
-    console.log ("need gas asap ")
+    console.log ("need gas asap ");
+
 }
