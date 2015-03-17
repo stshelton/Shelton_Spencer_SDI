2439a2952ec4aaa567e7e3656974621e9a1de67c
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-17 16:42:26 -0400

    return variable from function

--- day 7/js/arguments.js
+++ day 7/js/arguments.js
@@ -27,3 +27,27 @@ var width = prompt("enter width");
 var height = prompt("enter a height");
 
 calcArea(width,height);
+
+
+//return a value from our function to our main code
+
+function calcArea2(w,h)
+{
+    var area = w*h;
+    console.log("inside the function the area is"+area);
+
+    return area;
+}
+
+
+
+//console log the area
+//console.log(area);
+//this wont work area in "vegas"
+
+//need to return area;
+// and create a variable to catch the return value
+
+var rectArea = calcArea2(10,20)
+
+console.log("outside of the function after returning it the area is  "+rectArea);
