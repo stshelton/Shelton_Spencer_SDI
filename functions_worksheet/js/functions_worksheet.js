dc5cfc35c4ead78da74080010edefc234e68a128
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-17 18:36:54 -0400

    added comments

--- functions_worksheet/js/functions_worksheet.js
+++ functions_worksheet/js/functions_worksheet.js
@@ -6,23 +6,36 @@
  */
 
 
-
+//prompt
 var radius = prompt ("what is the radius of the circle")
-
+//function to find circumference
 function circumferenceOfCircle(radius)
 {
+    //math
     var diameter = 2 * radius;
     var circumference = diameter * Math.PI;
 
-
+//display
     console.log("the circumference of the circle is "+circumference);
-
+//return circumference
     return circumference;
 }
-
+//catch circumference in variable and call function
 var catchCircumference = circumferenceOfCircle(radius);
 
+//display outside of function
 console.log("your circumference out of the function is "+catchCircumference);
 
+//prompt
 var weight = prompt("how much do you weigh")
 
+function beesToDeath(w)
+{
+    var totalNumber= w * 8.666666667
+
+    console.log("If you weighed"+weight+"ilbs then it would take you"+totalNumber"of bees ")
+    return totalNumber;
+
+}
+
+var bees = beesToDeath(weight)
