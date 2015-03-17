039a49e6ab0f30f00aa939b1e773bbbcf945631a
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-17 18:29:44 -0400

    circumference of circle done

---
+++ functions_worksheet/js/functions_worksheet.js
@@ -0,0 +1,25 @@
+/*
+ Spencer Shelton
+ 3/17/15
+ section 00
+ functions worksheet
+ */
+
+
+
+var radius = prompt ("what is the radius of the circle")
+
+function circumferenceOfCircle(radius)
+{
+    var diameter = 2 * radius;
+    var circumference = diameter * Math.PI;
+    
+
+    console.log("the circumference of the circle is "+circumference);
+
+    return circumference;
+}
+
+var catchCircumference = circumferenceOfCircle(radius);
+
+console.log("your circumference out of the function is "+catchCircumference);
--- loops_worksheet/js/loops.js
+++ loops_worksheet/js/loops.js
@@ -6,15 +6,7 @@
  */
 
 
-//prompt
-var milesOnCar = prompt("how many miles do you have on your car");
-//user validation
-while(milesOnCar == "" || isNaN(milesOnCar))
-{
-    milesOnCar = prompt("Please do not leave blank and type a number. How many miles do you hav on your car?");
-}
-//display on screen
-console.log("you have "+milesOnCar+" miles on your car.")
+
 
 //next prompt
 var oilChange = prompt("When was the last time you changed your oil");
@@ -40,14 +32,27 @@ while( oilChange <= 100000)
 
 var milesDriven = [];
 
+//for loop to put amount of miles they drove each day into an array to latter be added togther
 for(var i = 1;i<8;i++)
 {
-    milesDriven[i]= prompt("how many miles did you drive on day #"+i)
+    milesDriven[i]= prompt("how many miles did you drive on day #"+i);
+    //user validation
     while(milesDriven[i] == ""||isNaN(milesDriven[i]))
     {
-        milesDriven[i] = prompt("please type a number and do not leave blank. How many miles did you drive on day #"+i)
+        milesDriven[i] = prompt("please type a number and do not leave blank. How many miles did you drive on day #"+i);
     }
 }
 console.log(milesDriven);
+
+//for loop to added all values together in array
+for(var i= 0; i<milesDriven.length; i++)
+{
+    var total= parseInt(milesDriven)[i] + parseInt(total);
+}
+console.log(total);
+
+var averagePerWeek = parseInt(total)/7;
+
+console.log("your average miles driven in a week is "+averagePerWeek);
 
 
