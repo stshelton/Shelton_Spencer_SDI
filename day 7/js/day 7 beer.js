d08344224f6f50b29b28ea0150c974977c0d1412
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-17 13:55:44 -0400

    day 7 loop for 99 bottles

--- day 7/js/day 7 beer.js
+++ day 7/js/day 7 beer.js
@@ -13,6 +13,15 @@
 
 for(var i =99; i>0; i--)
 {
-    console.log(i+" bottles of beer on the wall. "+i+"bottles of beer. you take one down and pass it around." +(--i)+"bottles of beer on the wall")
+    if(i===1) {
+        console.log(i + " bottles of beer on the wall. " + i + "bottles of beer. you take one down and pass it around. No more bottles of beer on the wall");
+    }
+
+
+    else
+    {
+        console.log(i + " bottles of beer on the wall. " + i + "bottles of beer. you take one down and pass it around." + (--i) + "bottles of beer on the wall");
+
+    }
 
 }
