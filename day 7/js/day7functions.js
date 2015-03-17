7bb3eb7858a4f98096875699b17e88e5e2024e83
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-17 15:18:41 -0400

    started functions
    got ahead of my self used parameters

---
+++ day 7/js/day7functions.js
@@ -0,0 +1,36 @@
+/*
+ Spencer Shelton
+ 3/17/15
+ section 00
+ day 7 functions
+ */
+
+
+
+// the basic structure of  function
+// function functionName(){code to run}
+
+//Create a function that will console log out hello
+
+function printHello(){
+
+    console.log("hello!")
+}
+
+//function call the print hello function
+// functionname();
+
+printHello();
+
+
+
+//using parameters
+function name (spencer, rime ){
+    var total = spencer + rime
+    console.log(total);
+
+}
+
+name(4,5);
+
+
