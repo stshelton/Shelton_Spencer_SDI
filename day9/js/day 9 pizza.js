5b834e9830223b1bb434679c51eb8eca620fba8a
stshelton <spencershelton1@Spencers-MacBook-Pro-2.local>
2015-03-26 13:32:13 -0400

    prompt

--- day9/js/day 9 pizza.js
+++ day9/js/day 9 pizza.js
@@ -5,3 +5,25 @@
  pizza
  */
 
+
+
+//figure out how much a pizza costs per slice and cost per sq in
+
+//prompt the user
+
+var pizzaRadius = prompt("welcome, lets calculate the cost of your pizza! what is the radius of your pizza.");
+
+//validation
+while(pizzaRadius===""||isNaN(pizzaRadius))
+{
+    pizzaRadius = prompt("please do not leave blank and only use numbers\n What is the radius of your pizza");
+
+}
+
+//prompt the user for cost of whole pizza
+do {
+    var pizzaCost = prompt("how much does your pizza cost in total");
+
+}while(pizzaCost ===""||isNaN(pizzaCost));
+
+
